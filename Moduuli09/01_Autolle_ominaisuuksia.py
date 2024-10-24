82b87c618dcc6c0cc83cacabcee1ed3a356a7abb
Tontsa <tontsa@nic.fi>
2024-10-24 14:28:41 +0300

    Update 01_Autolle_ominaisuuksia.py
    Updated class and print

--- Moduuli09/01_Autolle_ominaisuuksia.py
+++ Moduuli09/01_Autolle_ominaisuuksia.py
@@ -1,3 +1,12 @@
 # Ohjelmisto 2 - Moduuli 9 - Tehtävä 1 - Auto-olioille ominaisuuksia
 
+class Auto:
+    def __init__(self, rekisteritunnus: str, huippunopeus: int, nopeus=0, kuljettumatka=0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.kuljettumatka = kuljettumatka
 
+turbosaabi = Auto("ABC-123", 142)
+
+print(f"Auton tiedot\nRekisteritunnus: {turbosaabi.rekisteritunnus:s}\nHuippunopeus: {turbosaabi.huippunopeus}\nNopeus nyt: {turbosaabi.nopeus}\nKuljettu matka: {turbosaabi.kuljettumatka}")
