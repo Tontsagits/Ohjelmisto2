6d556b334e421bdff7354377a53a09d7985c8daf
Tontsa <tontsa@nic.fi>
2024-10-25 13:50:48 +0300

    Update 04_ajetaan_kilpaa.py
    added PrettyTable

--- Moduuli09/04_ajetaan_kilpaa.py
+++ Moduuli09/04_ajetaan_kilpaa.py
@@ -3,40 +3,46 @@
 from random import randint, randrange
 from time import sleep
 import os
+# from prettytable import PrettyTable
 
 class Auto:
 
-    tdelay = 0.5
+    tdelay = 0.1
 
     def __init__(self, rekisteritunnus: str, huippunopeus: int, nopeus=0, kuljettumatka=0):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
         self.nopeus = nopeus
         self.kuljettumatka = kuljettumatka
-        print(f"Auto {self.rekisteritunnus} lisätty.")
-        print(f"Auton tiedot\nRekisteritunnus: {self.rekisteritunnus:s}\nHuippunopeus: {self.huippunopeus}\nNopeus nyt: {self.nopeus}\nKuljettu matka: {self.kuljettumatka}")
-        sleep(Auto.tdelay)
-        os.system('cls')
+        # print(f"Auto {self.rekisteritunnus} lisätty.")
+        # print(f"Auton tiedot\nRekisteritunnus: {self.rekisteritunnus:s}\nHuippunopeus: {self.huippunopeus}\nNopeus nyt: {self.nopeus}\nKuljettu matka: {self.kuljettumatka}")
+        # sleep(Auto.tdelay)
+        # os.system('cls')
 
     def kiihdytä(self, nopmuutos: int):
         if 0 <= self.nopeus + nopmuutos <= self.huippunopeus:
             self.nopeus += nopmuutos
-            print(f"Auton {self.rekisteritunnus} nopeus on nyt {self.nopeus} km/h.")
+            # print(f"Auton {self.rekisteritunnus} nopeus on nyt {self.nopeus} km/h.")
         elif self.nopeus + nopmuutos < 0:
-            print(f"Auto {self.rekisteritunnus} on paikoillaan eikä liiku.")
+            # print(f"Auto {self.rekisteritunnus} on paikoillaan eikä liiku.")
             self.nopeus = 0
         else:
-            print(f"Auton {self.rekisteritunnus} huippunopeus {self.huippunopeus} km/h saavutettu.")
+            # print(f"Auton {self.rekisteritunnus} huippunopeus {self.huippunopeus} km/h saavutettu.")
             self.nopeus = self.huippunopeus
-        sleep(Auto.tdelay)
-        os.system('cls')
+        # sleep(Auto.tdelay)
+        # os.system('cls')
 
     def kulje(self, aikatuntia: float):
         self.kuljettumatka += self.nopeus * aikatuntia
-        print(f"Auto {self.rekisteritunnus} kulkee {self.nopeus} km/h nopeudella {aikatuntia:.2f} tuntia.\nYhteensä {self.nopeus * aikatuntia:.2f} kilometriä.\nAuto on nyt liikkunyt yhteensä {self.kuljettumatka:.2f} kilometriä.")
-        sleep(Auto.tdelay)
-        os.system('cls')
+        # print(f"Auto {self.rekisteritunnus} kulkee {self.nopeus} km/h nopeudella {aikatuntia:.2f} tuntia.\nYhteensä {self.nopeus * aikatuntia:.2f} kilometriä.\nAuto on nyt liikkunyt yhteensä {self.kuljettumatka:.2f} kilometriä.")
+        # sleep(Auto.tdelay)
+        # os.system('cls')
 
+
+print("Autokilpailu 1.0")
+sleep(3)
+os.system('cls')
+sleep(1)
 autot = []
 for i in range(1,11):
     autot.append(Auto(f"ABC-{i}", randint(100, 200)))
@@ -54,3 +60,11 @@ while True:
 
 for auto in autot:
     print(f"Auton tiedot\nRekisteritunnus: {auto.rekisteritunnus:s}\nHuippunopeus: {auto.huippunopeus}\nNopeus nyt: {auto.nopeus}\nKuljettu matka: {auto.kuljettumatka}")
+
+
+'''result = PrettyTable()
+result.field_names = ["Rekkari","Huippunopeus","Nopeus nyt","Kuljettu matka"]
+for auto in autot:
+    result.add_row(auto)
+print(result)
+'''
