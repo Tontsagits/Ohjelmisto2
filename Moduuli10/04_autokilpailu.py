437ea3d99501eab29b37c4aa5ac6bb4ccca3e065
Tontsa <tontsa@nic.fi>
2024-10-26 19:31:24 +0300

    upd
    upd

--- Moduuli10/04_autokilpailu.py
+++ Moduuli10/04_autokilpailu.py
@@ -16,7 +16,14 @@ for i in range(1,11):
 kilpailu1 = kilpailut.Kilpailu('Suuri romuralli', 8000, osallistujat)
 
 # Suoritetaan kilpailu
-for auto in osallistujat:
-    kilpailu1.tunti_kuluu(auto)
-kilpailu1.kilpailu_ohi()
-kilpailu1.tulosta_tilanne()
+while True:
+    for auto in osallistujat:
+        # Ajetaan 1h kerralla, auto muuttaa vauhtia ja liikkuu
+        kilpailu1.tunti_kuluu(auto, randint(-10,15))
+        # Tarkastetaan onko joku jo maalissa
+        kilpailu1.kilpailu_ohi(auto)
+
+        # Tulostetaan tilanne 10h välein
+        kilpailu1.tulosta_tilanne(auto)
+    if perilla:
+        break
--- Moduuli10/kilpailut.py
+++ Moduuli10/kilpailut.py
@@ -1,15 +1,31 @@
+from prettytable import PrettyTable
+
 class Kilpailu:
 
-    def __init__(self, nimi: str, pituuskm: int, autot: list):
+    def __init__(self, nimi: str, pituuskm: int, osallistujat: list):
         self.nimi = nimi
         self.pituuskm = pituuskm
-        self.autot = autot
+        self.osallistujat = osallistujat
 
-    def tunti_kuluu(self):
+    def tunti_kuluu(self, auto, nopmuutos, aika=1):
+        auto.kiihdytä(nopmuutos)
+        auto.kulje(aika)
         return
 
-    def tulosta_tilanne(self):
+    def tulosta_tilanne(self, auto):
+        result = PrettyTable(padding_width=5)
+        result.field_names = ["Rekkari", "Huippunopeus", "Nopeus nyt", "Kuljettu matka"]
+        result.sortby = "Kuljettu matka"
+        rivi = []
+        rivi.append(auto.rekisteritunnus)
+        rivi.append(auto.huippunopeus)
+        rivi.append(auto.nopeus)
+        rivi.append(auto.kuljettumatka)
+        result.add_row(rivi)
+        print(result)
         return
 
-    def kilpailu_ohi(self):
+    def kilpailu_ohi(self, auto):
+        if auto.kuljettumatka >= self.pituuskm:
+            return False
         return
