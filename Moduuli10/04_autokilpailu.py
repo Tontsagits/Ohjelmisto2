46f5c96042e99dd7e05dc937d53e324c26a22eec
Tontsa <tontsa@nic.fi>
2024-10-26 19:55:31 +0300

    upd
    Solved

--- Moduuli10/04_autokilpailu.py
+++ Moduuli10/04_autokilpailu.py
@@ -14,6 +14,9 @@ for i in range(1,11):
 # Luodaan kilpailu
 kilpailu1 = kilpailut.Kilpailu('Suuri romuralli', 8000, osallistujat)
 
+# Tulostetaan lähtötilanne
+kilpailu1.tulosta_tilanne(osallistujat)
+
 # Suoritetaan kilpailu
 while True:
     kesto = 0
@@ -21,13 +24,18 @@ while True:
         # Ajetaan 1h kerralla, auto muuttaa vauhtia ja liikkuu
         kilpailu1.tunti_kuluu(auto, randint(-10,15))
         kesto += 1
+
         # Tarkastetaan onko joku jo maalissa, jos on lopetetaan
-        kilpailu1.kilpailu_ohi(auto)
+        if kilpailu1.kilpailu_ohi(auto):
+            perilla = True
+            break
 
         # Tulostetaan tilanne 10h välein
-        for auto in osallistujat:
-            kilpailu1.tulosta_tilanne(auto)
+        if kesto % 10 == 0:
+            kilpailu1.tulosta_tilanne(osallistujat)
     if perilla:
         break
 
 # Tulostetaan tulokset
+print("Valmista tuli!")
+kilpailu1.tulosta_tilanne(osallistujat)
--- Moduuli10/kilpailut.py
+++ Moduuli10/kilpailut.py
@@ -7,25 +7,25 @@ class Kilpailu:
         self.pituuskm = pituuskm
         self.osallistujat = osallistujat
 
-    def tunti_kuluu(self, auto, nopmuutos, aika=1):
+    def tunti_kuluu(self, auto, nopmuutos: int, aika=1):
         auto.kiihdytä(nopmuutos)
         auto.kulje(aika)
         return
 
-    def tulosta_tilanne(self, auto):
+    def tulosta_tilanne(self, osallistujat: list):
         result = PrettyTable(padding_width=5)
         result.field_names = ["Rekkari", "Huippunopeus", "Nopeus nyt", "Kuljettu matka"]
-        result.sortby = "Kuljettu matka"
         rivi = []
-        rivi.append(auto.rekisteritunnus)
-        rivi.append(auto.huippunopeus)
-        rivi.append(auto.nopeus)
-        rivi.append(auto.kuljettumatka)
-        result.add_row(rivi)
+        for auto in osallistujat:
+            rivi.append(auto.rekisteritunnus)
+            rivi.append(auto.huippunopeus)
+            rivi.append(auto.nopeus)
+            rivi.append(auto.kuljettumatka)
+            result.add_row(rivi)
         print(result)
         return
 
     def kilpailu_ohi(self, auto):
         if auto.kuljettumatka >= self.pituuskm:
-            return False
-        return
+            return True
+        return False
