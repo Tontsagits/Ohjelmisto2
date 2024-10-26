7b50a6a38f1f495c3fab1db45e16dd29ce0b2d41
Tontsa <tontsa@nic.fi>
2024-10-26 18:20:59 +0300

    upd
    upd

--- Moduuli10/04_autokilpailu.py
+++ Moduuli10/04_autokilpailu.py
@@ -3,13 +3,14 @@
 import autot
 import kilpailut
 
-from random import randint, randrange
-from prettytable import PrettyTable
+from random import randint
+#from prettytable import PrettyTable
 
 osallistujat = []
 for i in range(1,15):
     nopeus = randint(100,250)
     osallistujat.append(autot.Auto(f"ABC-{i}", nopeus))
+
 
 kilpailut.Kilpailu('Suuri romuralli', 8000, osallistujat)
 
