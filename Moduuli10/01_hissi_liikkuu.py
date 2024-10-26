2e3f28bf5c184bdf7a0926d389e4875de7a73005
Tontsa <tontsa@nic.fi>
2024-10-26 13:21:39 +0300

    Created 01_hissi
    Created 01_hissi

--- Moduuli10/01_hissi_liikkuu.py
+++ Moduuli10/01_hissi_liikkuu.py
@@ -1,5 +1,32 @@
 # Ohjelmisto 2 - Moduuli 10 - Tehtävä 1 - Hissi liikkuu ylös ja alas
 
+class Hissi:
+    def __init__(self, alin: int, ylin: int):
+        self.alin = alin
+        self.ylin = ylin
+        self.kerros = alin
 
+    def siirry_kerrokseen(self, kohde):
+        if kohde > self.ylin or kohde < self.alin:
+            print(f"Ei sellaista kerrosta ({kohde}).")
+            return
+        print(f"Kerros: {self.kerros}")
+        while kohde > self.kerros:
+            self.kerros_ylös()
+        while kohde < self.kerros:
+            self.kerros_alas()
+        return
+    def kerros_ylös(self):
+        self.kerros += 1
+        print(f"Kerros: {self.kerros}")
+        return
+    def kerros_alas(self):
+        self.kerros -= 1
+        print(f"Kerros: {self.kerros}")
+        return
 
+# Main
 
+hiss1 = Hissi(1, 9)
+hiss1.siirry_kerrokseen(9)
+hiss1.siirry_kerrokseen(34)
