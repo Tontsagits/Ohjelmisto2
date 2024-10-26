c485756e7fc3d460a23be58b9993c1c9c79bc9ad
Tontsa <tontsa@nic.fi>
2024-10-26 15:56:03 +0300

    Updated classes
    Fixed some bugs.

--- Moduuli10/02_talon_hissit.py
+++ Moduuli10/02_talon_hissit.py
@@ -1,4 +1,4 @@
-# Ohjelmisto 2 - Moduuli 10 - Tehtävä 1 - Hissi liikkuu ylös ja alas
+# Ohjelmisto 2 - Moduuli 10 - Tehtävä 2 - Talo ja sen hissit
 
 class Hissi:
     def __init__(self, alin: int, ylin: int):
@@ -25,8 +25,16 @@ class Hissi:
         print(f"Kerros: {self.kerros}")
         return
 
+class Talo:
+    def __init__(self, alin: int, ylin: int, kpl: int):
+        self.alin = alin
+        self.ylin = ylin
+        self.kpl = kpl
+        self.hissit = [Hissi(self.alin, self.ylin) for _ in range(self.kpl)]
+    def aja_hissiä(self, hissi: int, kohdekerros: int):
+        self.hissit[hissi].siirry_kerrokseen(kohdekerros)
+
 # Main
 
-hiss1 = Hissi(1, 9)
-hiss1.siirry_kerrokseen(9)
-hiss1.siirry_kerrokseen(34)
+talo1 = Talo(1, 9, 3)
+talo1.aja_hissiä(0, 5)
