5068a1f2d1e19e127f60a51492590de122de6d42
Tontsa <tontsa@nic.fi>
2024-10-30 11:33:32 +0200

    Create API_testing_1.py
    testing API

---
+++ python-test/API_testing_1.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+hakusana = input("Anna hakusana: ")
+
+# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+
+vastaus = requests.get(pyyntö).json()
+
+# print(json.dumps(vastaus, indent=4))
+
+# Käy läpi JSON rakennetta. Sisältää dictionary elementtejä sisäkkäin ja listoja
+for entry in vastaus:
+    print(f"{entry['show']['name']}: {entry['show']['type']}")
+    print(f" - Rating: {entry['show']['rating']['average']}")
+    print(f" - Schedule: {entry['show']['schedule']['days']}")
+    # Tulostetaan JSON tiedosta aikataulu, jos sellainen on, muoto JSON on lista
+    for day in entry['show']['schedule']['days']:
+        print(f" - Schedule: {day}")
