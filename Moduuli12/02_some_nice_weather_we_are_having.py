3269daa215272a308344741c837c594fdfaee777
Tontsa <tontsa@nic.fi>
2024-11-02 12:49:25 +0200

    Update 02_some_nice_weather_we_are_having.py
    Solved

--- Moduuli12/02_some_nice_weather_we_are_having.py
+++ Moduuli12/02_some_nice_weather_we_are_having.py
@@ -3,6 +3,7 @@
 from colorama import Fore
 import requests
 import os
+import time
 
 # API
 # Current weather data
@@ -30,18 +31,23 @@ api_url = "https://api.openweathermap.org/data/2.5/weather?q"
 api_key = "8891ed0ba67dbb011bea8d0213972a42"
 api_lang = "fi"
 api_units ="metric"
-city_name = str(input(f"*** {Fore.LIGHTBLUE_EX}Anna {Fore.RED}kaupunki{Fore.LIGHTBLUE_EX}, jonka säätiedot haluat hakea: {Fore.RESET}"))
+print()
+city_name = str(input(f"   *** {Fore.LIGHTBLUE_EX}Anna {Fore.RED}kaupunki{Fore.LIGHTBLUE_EX}, jonka säätiedot haluat hakea: {Fore.RESET}"))
 api_req = f"{api_url}={city_name}&appid={api_key}&lang={api_lang}&units={api_units}"
 api_resp = requests.get(api_req).json()
-print()
-print(api_resp)
 '''print()
-print(f"{Fore.LIGHTBLUE_EX}Kaupunki: {Fore.GREEN}{api_resp['name']}")
-print(f"{Fore.LIGHTBLUE_EX}Lämpötila: {Fore.GREEN}{api_resp['main']['temp']}{Fore.LIGHTBLUE_EX}'C")
-print(f"{Fore.LIGHTBLUE_EX}Vuorokauden")
-print(f"{Fore.LIGHTBLUE_EX}- Alin lämpötila: {Fore.GREEN}{api_resp['main']['temp_min']}{Fore.LIGHTBLUE_EX}'C")
-print(f"{Fore.LIGHTBLUE_EX}- Ylin lämpötila: {Fore.GREEN}{api_resp['main']['temp_max']}{Fore.LIGHTBLUE_EX}'C")
-print(f"{Fore.LIGHTBLUE_EX}Ilmanpaine: {Fore.GREEN}{api_resp['main']['pressure']} {Fore.LIGHTBLUE_EX}hPa")
-print(f"{Fore.LIGHTBLUE_EX}Ilmankosteusprosentti: {Fore.GREEN}{api_resp['main']['humidity']} {Fore.LIGHTBLUE_EX}%")
-print(f"{Fore.LIGHTBLUE_EX}Tuulen nopeus: {Fore.GREEN}{api_resp['wind']['speed']} {Fore.LIGHTBLUE_EX}m/s")
+for key, value in api_resp.items():
+    print(key, value)
 '''
+print()
+print(f"{Fore.LIGHTBLUE_EX} Kaupunki: {Fore.GREEN}{api_resp['name']}")
+print(f"{Fore.LIGHTBLUE_EX}  Säätila: {Fore.GREEN}{api_resp['weather'][0]['description']}")
+print(f"{Fore.LIGHTBLUE_EX}  Lämpötila: {Fore.GREEN}{api_resp['main']['temp']}{Fore.LIGHTBLUE_EX}'C")
+print(f"{Fore.LIGHTBLUE_EX}  Vuorokauden")
+print(f"{Fore.LIGHTBLUE_EX}  - Alin lämpötila: {Fore.GREEN}{api_resp['main']['temp_min']}{Fore.LIGHTBLUE_EX}'C")
+print(f"{Fore.LIGHTBLUE_EX}  - Ylin lämpötila: {Fore.GREEN}{api_resp['main']['temp_max']}{Fore.LIGHTBLUE_EX}'C")
+print(f"{Fore.LIGHTBLUE_EX}  Ilmanpaine: {Fore.GREEN}{api_resp['main']['pressure']} {Fore.LIGHTBLUE_EX}hPa")
+print(f"{Fore.LIGHTBLUE_EX}  Ilmankosteusprosentti: {Fore.GREEN}{api_resp['main']['humidity']} {Fore.LIGHTBLUE_EX}%")
+print(f"{Fore.LIGHTBLUE_EX}  Tuulen nopeus: {Fore.GREEN}{api_resp['wind']['speed']} {Fore.LIGHTBLUE_EX}m/s{Fore.RESET}")
+print()
+time.sleep(5)
