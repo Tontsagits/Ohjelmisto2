565c3969c33d60118cdab83ba72047135c3220c2
Tontsa <tontsa@nic.fi>
2024-11-20 21:47:45 +0200

    Testing API Endpoints
    Testing API Endpoints

--- python-test/API_ENDPOINT_testing_2_JSON.py
+++ python-test/API_ENDPOINT_testing_2_JSON.py
@@ -1,22 +1,25 @@
-# API Endpoint testing 1 - Flask and parameters after ? mark in URL
+# API Endpoint testing 2 - JSON return
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-# määritetään endpoint eli päätepiste
 @app.route('/summa')
-# summa funktio, ajetaan kun kutsutaan endpointtia summa
 def summa():
     args = request.args
     luku1 = float(args.get("luku1"))
     luku2 = float(args.get("luku2"))
     summa = luku1 + luku2
-    return str(summa)
+
+    vastaus = {
+        "luku1": luku1,
+        "luku2": luku2,
+        "summa": summa
+    }
+
+    return vastaus
 
 
 if __name__ == '__main__':
-    # käynnistetään taustapalvelu
-    # voisi olla myös 'localhost'
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
---
+++ python-test/API_ENDPOINT_testing_3_parameters.py
@@ -0,0 +1,17 @@
+# API Endpoint testing 3 - parameters in URL
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/kaiku/<teksti>')
+def kaiku(teksti):
+    vastaus = {
+        "kaiku": teksti + " " + teksti
+    }
+    return vastaus
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
