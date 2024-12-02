05083826b83e73eb019a4cd78a22e80246dcc136
Tontsa <tontsa@nic.fi>
2024-12-02 15:01:06 +0200

    Updated
    Updated

--- js-testing/index.js
+++ js-testing/index.js
@@ -2,8 +2,29 @@
 
 console.log('Hello Javascript World!');
 
+let muuttuja;
+muuttuja = 'Hank Moody';
+console.log(muuttuja);
+
+const vakio = 'Another Hank Moody';
+console.log(vakio);
+
 alert('Hello JavaScript World from here too!');
 
-const name = prompt('Anna nimesi: ');
-// const name = 'Hank Moody';
+const name = prompt('Anna nimesi:', 'Nimi tähän.');
 document.querySelector('#testingpara').innerHTML = 'Hello, ' + name + '!';
+
+const answer = confirm('No miten on? Kyllä vai ei?');
+console.log(answer);
+console.log(typeof answer);
+
+const ageInt = parseInt(prompt('Anna ikäsi:', 'Ikä tähän.'));
+console.log(ageInt);
+console.log(typeof ageInt);
+
+let first, second, third, all;
+first = 'Good ';
+second = 'morning ';
+third = 'all.';
+all = first + second + third;
+console.log(all);
