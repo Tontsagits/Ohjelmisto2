fbbacb266ff9f4311245e4951dac1565bbd2b99d
Tontsa <tontsa@nic.fi>
2024-12-02 14:25:52 +0200

    Updated
    Updated

--- js-testing/index.js
+++ js-testing/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log('Hello Javascript World!');
+
+alert('Hello JavaScript World from here too!');
+
+const name = prompt('Anna nimesi: ');
+// const name = 'Hank Moody';
+document.querySelector('#testingpara').innerHTML = 'Hello, ' + name + '!';
