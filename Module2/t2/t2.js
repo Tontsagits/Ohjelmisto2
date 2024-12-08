701416fedbd9fc4c6f862ff4b43d0221cddccced
Tontsa <tontsa@nic.fi>
2024-12-08 15:13:28 +0200

    Solved
    Solved

--- Module2/t2/t2.js
+++ Module2/t2/t2.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const numOfParticipants = parseInt(
+    prompt('Montako osallistujaa?', 'Osallistujien määrä tähän.'));
+
+// alustetaan lista
+const names = [];
+
+// lisätään nimet listaan
+for (let i = 1; i <= numOfParticipants; i++) {
+  let name = '';
+  name = prompt('Anna osallistujan nimi:', 'Osallistujan nimi tähän.');
+  names.push(name);
+}
+
+// järjestetään lista aakkosjärjestykseen
+names.sort();
+
+// luodaan apumuuttuja
+let namesStr = ``;
+
+// jokaista nimeä kohti lisätään nimi tekstikenttään LI tagien kanssa
+for (let name of names) {
+  console.log(name);
+  namesStr += `<li>${name}</li>`;
+}
+// lisätään syntynyt teksti HTML koodiin
+document.querySelector('#tag1').innerHTML = `${namesStr}`;
