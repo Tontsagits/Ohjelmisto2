9f1f79755581c7250aa0d4b48a4343bafef81113
Tontsa <tontsa@nic.fi>
2024-12-13 22:20:40 +0200

    updated
    updated

--- Moduuli13/02_mika_lentokenttä.py
+++ Moduuli13/02_mika_lentokenttä.py
@@ -1,18 +1,33 @@
 # Ohjelmisto 2 - Moduuli 13 - Tehtävä 2 - Mikä lentokenttä, backend versio
 
 import mysql.connector
+from flask import Flask
+
+
+app = Flask(__name__)
 
 game_airports = {}
 
-yhteys = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    database="rahtipeli",
-    user="pilotti",
-    password="pilotti12345",
-    autocommit=True,
-    collation='utf8mb4_unicode_ci'
-)
+# toimiva URL esim http://127.0.0.1:3000/airport?ident=<ident>
+@app.route('/airport')
+def airport():
+    args = request.args
+    ident = str(args.get("ident"))
+
+    return str(ident)
+
+
+# database connection
+def database();
+    yhteys = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        database="rahtipeli",
+        user="pilotti",
+        password="pilotti12345",
+        autocommit=True,
+        collation='utf8mb4_unicode_ci'
+    )
 
 
 # Function to get information of any given airport. Function return airport details (Name, city and country)
@@ -27,4 +42,5 @@ def get_information(ident: str):
         return (info[0], info[1])
 
 
-print(get_information("EFHK"))
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
